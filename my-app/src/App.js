1bdf4212ee0c5b6aaa9a734f67f04d11cb72225e
mirackarakasli <ilhansamikara@gmail.com>
2021-08-14 16:54:14 +0300

    React project 2. ders

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Card from "./components/cards/Card";
+import Header from "./components/header/Header";
+import Navbar from "./components/navbar/Navbar";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Header/>
+      <Card />
+
+    </div>
+  );
+};
+
+
+export default App;
---
+++ my-app/src/components/cards/Card.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { data } from "../../helpers/Data"
+import "./Card.css"
+
+function Card() {
+    return(
+        <div className="card-container">
+            {/* <div className="cards">
+                <div className="title">
+                    <h2>{data[0].title}</h2>
+                </div>
+                <img src={data[0].image} alt={data[0].title}/>
+                <div className="card-over">
+                    <p>{data[0].desc}</p>
+                </div>
+            </div> */}
+
+            {data.map((card) => {
+                return (
+                    <div className="cards" key={card.id}>
+                        <div className="title">
+                            <h2>{card.title}</h2>
+                        </div>
+
+                        <img src={card.image} alt={card.title}/>
+                        
+                        <div className="card_over">
+                            <p>{card.desc}</p>
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+
+
+export default Card;
